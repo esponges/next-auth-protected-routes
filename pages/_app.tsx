48366ba3a17f6ffb3b01e690a616531474c5444e
esponges <esponges@gmail.com>
2022-12-24 12:01:44 -0600

    main page

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,6 +1,7 @@
 import { ProtectedLayout } from '../components/layouts/protected';
 import '../styles/globals.css';
 import type { AppProps } from 'next/app';
+import { SessionProvider } from 'next-auth/react';
 
 // add requireAuth to AppProps
 type AppPropsWithAuth = AppProps & {
@@ -10,11 +11,13 @@ type AppPropsWithAuth = AppProps & {
 };
 
 export default function App({ Component, pageProps }: AppPropsWithAuth) {
-  return Component.requireAuth ? (
-    <ProtectedLayout>
+  return <SessionProvider session={pageProps.session}>
+    {Component.requireAuth ? (
+      <ProtectedLayout>
+        <Component {...pageProps} />
+      </ProtectedLayout>
+    ) : (
       <Component {...pageProps} />
-    </ProtectedLayout>
-  ) : (
-    <Component {...pageProps} />
-  );
+    )}
+  </SessionProvider>;
 }
