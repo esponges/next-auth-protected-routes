2a21194c52352e76cc1b47a345901a0316829b97
esponges <esponges@gmail.com>
2022-12-24 11:35:59 -0600

    protected layout

---
+++ components/layouts/protected.tsx
@@ -0,0 +1,41 @@
+import { useSession } from 'next-auth/react';
+import { useRouter } from 'next/router';
+import { useEffect } from 'react';
+
+type Props = {
+  children: React.ReactElement;
+};
+
+/*
+  add the requireAuth property to the page component
+  to protect the page from unauthenticated users
+  e.g.:
+  OrderDetail.requireAuth = true;
+  export default OrderDetail;
+ */
+
+export const ProtectedLayout = ({ children }: Props): JSX.Element => {
+  const router = useRouter();
+  const { status: sessionStatus } = useSession();
+  const authorized = sessionStatus === 'authenticated';
+  const unAuthorized = sessionStatus === 'unauthenticated';
+  const loading = sessionStatus === 'loading';
+
+  useEffect(() => {
+    if (loading || !router.isReady) return;
+
+    if (unAuthorized) {
+      console.log('not authorized');
+      router.push({
+        pathname: '/auth',
+        query: { returnUrl: router.asPath },
+      });
+    }
+  }, [loading, unAuthorized, sessionStatus, router]);
+
+  if (loading) {
+    return <>Loading app...</>;
+  }
+
+  return authorized ? <div>{children}</div> : <></>;
+};
